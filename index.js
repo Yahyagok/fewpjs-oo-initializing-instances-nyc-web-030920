dc5ebe2934f3366f2e99b08ecc14e908183ca5d8
Yahyagok <ygok17@hotmail.com>
2020-04-30 19:36:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your code here
+
+
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food 
+        this.drink = drink
+
+    }
+
+}
+
+//  let bfast = new Breakfast('eggs', 'juice')
+
+// let mediterranen = new Breakfast("eggs", "Ayran" )
+
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup 
+        this.drink = drink 
+
+    }
+}
+
+// let kuruFasulye  =new Lunch("Coban Salata", "lentil", "Ayran")
+// newlunch = new Lunch('side salad', 'broccoli cheddar soup', 'iced tea')
+
+class Dinner {
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad 
+        this.soup = soup 
+        this.entree = entree 
+        this._dessert = dessert 
+
+    }
+}
+
+
+// let adanaKebap = new Dinner("akdeniz salata", "ezo gelin", "aya kofte", "susamli pekmez")
+// newdinner = new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
+
+// class Dog { 
+//     constructor(name, breed){
+//         this.name = name;
+//         this.breed = breed;
+
+//     }
+// }
+
+// let bigFluffyDog1 = new Dog("Buzz", "greatPyrenees");
+
+// let bigFluffyDog2 = new Dog("Woody", "labrador");
